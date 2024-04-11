4d0f5ec784c217c8ec0bb0a62738e92c6f7ef8cc
rschuma <71301319+rschuma@users.noreply.github.com>
2024-04-11 13:35:53 -0400

    Update elizaNLP.py

--- elizaNLP.py
+++ elizaNLP.py
@@ -6,29 +6,29 @@ nlp = spacy.load("en_core_web_sm")
 
 fruit_descriptions = {
     "apple": ["sweet", "tart", "sour", "crisp", "green", "red", "medium", "round", "stem", "seeds"],
-    "mango": ["sweet", "soft", "tropical", "yellow", "medium", "orange", "red", "green", "round", "stem", "summer"],
+    "mango": ["sweet", "soft", "tropical", "yellow", "medium", "orange", "red", "green", "round", "stem"],
     "banana": ["sweet", "creamy", "yellow", "green", "medium", "long", "curved", "peel", "bunch", "tropical"],
-    "blueberry": ["sweet", "tart", "small", "blue", "berry", "summer"],
-    "blackberry": ["sweet", "tart", "small", "black", "blue", "berry", "thorns", "summer"],
-    "raspberry": ["sweet", "fuzzy", "small", "red", "thorns", "summer"],
-    "strawberry": ["sweet", "tart", "small", "red", "pink", "seeds", "stem", "summer", "spring"],
-    "cherry": ["sweet", "tart", "small", "red", "seeds", "stem", "bunch","round", "summer", "spring"],
-    "grape": ["sweet", "crisp", "small", "seedless", "purple", "green", "bunch", "round", "summer", "fall"],
-    "watermelon": ["sweet", "crisp", "refreshing", "big", "red", "green", "rind", "seeds", "round", "melon", "summer"],
-    "honeydew melon": ["sweet", "soft", "refreshing", "big", "green", "rind", "seeds", "round", "melon", "summer"],
-    "cantaloupe": ["sweet", "soft", "refreshing", "big", "orange", "rind", "seeds", "round", "melon", "summer"],
-    "pear": ["sweet", "tart", "soft", "medium", "green", "yellow", "stem", "seeds", "round", "summer", "fall"],
-    "plum": ["sweet", "tart", "soft", "medium", "purple", "pit", "round", "summer"],
-    "apricot": ["sweet", "soft", "medium", "fuzzy", "orange", "pit", "round", "summer"],
-    "peach": ["sweet", "soft", "medium", "fuzzy", "pink", "orange", "pit", "round", "summer", "spring"],
-    "kiwi": ["sweet", "tropical", "fuzzy", "refreshing", "green", "brown", "seeds", "round", "fall", "winter"],
-    "pomegranate": ["sweet", "tart", "tropical", "medium", "red", "pink", "seeds", "round", "fall"],
-    "pineapple": ["sweet", "tart", "tropical", "refreshing", "seedless", "spiky", "big" "yellow"],
-    "orange": ["sweet", "tart", "sour", "citrus", "medium", "orange", "round", "stem", "seeds", "winter"],
-    "grapefruit": ["sour", "tart", "citrus", "medium", "orange", "pink", "round", "stem", "seeds", "winter"],
+    "blueberry": ["sweet", "tart", "small", "blue", "berry"],
+    "blackberry": ["sweet", "tart", "small", "black", "blue", "berry", "thorns"],
+    "raspberry": ["sweet", "fuzzy", "small", "red", "thorns"],
+    "strawberry": ["sweet", "tart", "small", "red", "pink", "seeds", "stem"],
+    "cherry": ["sweet", "tart", "small", "red", "seeds", "stem", "bunch","round"],
+    "grape": ["sweet", "crisp", "small", "seedless", "purple", "green", "bunch", "round"],
+    "watermelon": ["sweet", "crisp", "refreshing", "big", "red", "green", "a rind", "seeds", "round", "melon"],
+    "honeydew melon": ["sweet", "soft", "refreshing", "big", "green", "a rind", "seeds", "round", "melon"],
+    "cantaloupe": ["sweet", "soft", "refreshing", "big", "orange", "a rind", "seeds", "round", "melon"],
+    "pear": ["sweet", "tart", "soft", "medium", "green", "yellow", "stem", "seeds", "round"],
+    "plum": ["sweet", "tart", "soft", "medium", "purple", "a pit", "round"],
+    "apricot": ["sweet", "soft", "medium", "fuzzy", "orange", "a pit", "round"],
+    "peach": ["sweet", "soft", "medium", "fuzzy", "pink", "orange", "a pit", "round"],
+    "kiwi": ["sweet", "tropical", "fuzzy", "refreshing", "green", "brown", "seeds", "round"],
+    "pomegranate": ["sweet", "tart", "tropical", "medium", "red", "pink", "seeds", "round"],
+    "pineapple": ["sweet", "tart", "tropical", "refreshing", "seedless", "spiky", "big", "yellow"],
+    "orange": ["sweet", "tart", "sour", "citrus", "medium", "orange", "round", "stem", "seeds"],
+    "grapefruit": ["sour", "tart", "citrus", "medium", "orange", "pink", "round", "stem", "seeds"],
     "lemon": ["sour", "tart", "citrus", "medium", "refreshing", "yellow", "round", "seeds"],
     "lime": ["sour", "tart", "citrus", "medium", "refreshing", "green", "round", "seeds"],
-    "dragon fruit": ["sweet", "tropical", "seeds", "spiky", "medium" "pink", "green", "white"]
+    "dragon fruit": ["sweet", "tropical", "seeds", "spiky", "medium", "pink", "green", "white"]
 }
 # apple, mango, banana, blueberry, blackberry, raspberry, strawberry, cherry, grape, watermelon, honeydew melon, cantaloupe, pear, plum, apricot, peach, kiwi, pomegranate, pineapple, orange, grapefruit, lemon, lime, dragon fruit
 
@@ -51,9 +51,7 @@ def analyze_response(response):
     
     affirmative = any(token.lemma_ in ["yes", "affirmative", "indeed", "absolutely", "certainly", "sure", "definitely", "of course", "yeah", "yep", "yup", "aye", "roger", "uh-huh", "right", "okay", "agreed", "true", "yea", "correct", "alright", "amen", "positively", "undoubtedly", "yah", "yass", "exactly", "naturally", "precisely", "assuredly", "aha", "agreed", "granted", "undoubtedly", "unquestionably", "yesh"] for token in doc)
 
-    negative = any(token.lemma_ in ["no", "negative", "nay", "nope", "nah", "no way", "not", "never", "nix", "uh-uh", "nope", "not at all", "absolutely not", "by no means", "not a chance", "decline", "refuse", "reject", "deny", "veto", "rebuff", "renounce", "repudiate", "retract", "revoke", "withdraw", "non", "none", "nothing", "nowhere", 
-    "neither", "null", "void", "zero"] for token in doc)
-
+    negative = any(token.lemma_ in ["no", "negative", "nay", "nope", "nah", "no way", "not", "never", "nix", "uh-uh", "nope", "not at all", "absolutely not", "by no means", "not a chance", "decline", "refuse", "reject", "deny", "veto", "rebuff", "renounce", "repudiate", "retract", "revoke", "withdraw", "non", "none", "nothing", "nowhere", "neither", "null", "void", "zero"] for token in doc)
     unsure = any(token.lemma_ in ["unsure", "maybe", "perhaps", "possibly", "probably", "doubtful", "dubious", "questionable", "uncertain", "undecided", "undetermined", "don't know", "dont know", "idk"] for token in doc)
 
     return affirmative, negative, unsure
